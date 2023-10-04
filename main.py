df76c14fd392510645875dec8edabdf28eff54f8
Timothée CASINI <timothee.casini@hotmail.fr>
2023-10-04 12:12:10 +0200

    modifs

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ option = st.selectbox('Choix du descripteur',list_descripteur)
 st.write(des.présentation(option))
 
 #Selection de la valeur souhaité du descripteur
-valeur = st.slider("Quel valeur souhaitez-vous pour votre produit pour le descripteur "+option+" ?",0,10)
+valeur = st.slider("Quel valeur souhaitez-vous pour votre produit pour le descripteur " + str (option) + " ?",0,10)
 
 #Recupere les modèles voulus
 equation,parametre = des.recupModele(option,valeur)
-st.write("Le modèle optimale pour le descripteur "+option+" à la valeur "+str(valeur)+" requiert ces mesures : "+parametre)
+st.write("Le modèle optimale pour le descripteur " + str(option) + " à la valeur "+str(valeur)+" requiert ces mesures : " + str(parametre))
 st.write(equation)
 
 
